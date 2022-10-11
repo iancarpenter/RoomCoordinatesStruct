037eeb0b0fbdf199355096db8bd73d7dbc167ca0
iancarpenter <ianhcarpenter@gmail.com>
2022-10-11 19:51:43 +0100

    Add project files.

---
+++ RoomCoordinatesStruct/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+
+namespace RoomCoordinatesStruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Coordinate a = new Coordinate(3, 3);
+            Coordinate b = new Coordinate(2, 3);
+            Coordinate c = new Coordinate(2, 2);
+
+            Console.WriteLine(Coordinate.AreAdjacent(a, b));
+            Console.WriteLine(Coordinate.AreAdjacent(b, c));
+            Console.WriteLine(Coordinate.AreAdjacent(a, c));
+            Console.WriteLine(Coordinate.AreAdjacent(a, a));
+
+        }
+    }
+
+    internal struct Coordinate
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Coordinate(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public static bool AreAdjacent(Coordinate a, Coordinate b)
+        {
+            int rowChange = a.Row - b.Row;
+            int columnChange = a.Column - b.Column;
+
+            if (Math.Abs(rowChange) <= 1 && columnChange == 0) return true; 
+            if (Math.Abs(columnChange) <= 1 && rowChange == 0) return true;
+
+            return false;
+        }
+    }
+}
